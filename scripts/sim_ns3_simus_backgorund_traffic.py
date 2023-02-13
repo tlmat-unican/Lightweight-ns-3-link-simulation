9973e69abf6f92e6055fb703991286e05650940c
fkhan <fatima.khan@alumnos.unican.es>
2023-02-13 12:04:12 +0100

    Scripts mod

--- scripts/sim_ns3_simus_backgorund_traffic.py
+++ scripts/sim_ns3_simus_backgorund_traffic.py
@@ -1,4 +1,6 @@
-
+'''
+Script to analyse the variation in background traffic state. Modify Nsim to adjust the number of simulations.
+'''
 
 import os
 import time 
@@ -61,28 +63,27 @@ def StopNs3 (scenario) :
 def main():
     print("Start sim")
 
-    if(Simulation_name == "Backtraffic"):
-      os.system("mkdir {}/BackTrafficv2/".format(OutcomeFolder))
-      for sim_rate in range(5,31,1):
-        Sim_name = "{}_finito_1BDPConfigScenario{}_sims".format(Transport_model,sim_rate)
-        cmd = 'mkdir ./{}/BackTrafficv2/{}/'.format(OutcomeFolder,Sim_name)
+    os.system("mkdir {}/BackTrafficv2/".format(OutcomeFolder))
+    for sim_rate in range(5,31,1):
+      Sim_name = "{}_finito_1BDPConfigScenario{}_sims".format(Transport_model,sim_rate)
+      cmd = 'mkdir ./{}/BackTrafficv2/{}/'.format(OutcomeFolder,Sim_name)
+      os.system(cmd)
+
+      if Sim_name!="":
+        Config_Out="--out-folder-path={}".format("../../ns3_sim/" + Sim_name+ "/") 
+      with open("../ns-allinone-3.35/ns-3.35/scratch/ConfigScenario_template.json", "r") as jsonFileReceiver:
+        data = json.load(jsonFileReceiver)
+      print(data["Links"][1]["BackFeatures"]["DataRate"] )
+      data["Links"][1]["BackFeatures"]["DataRate"] = sim_rate
+
+      with open("../ns-allinone-3.35/ns-3.35/scratch/ConfigScenario.json", "w") as jsonFileReceiver:
+                  json.dump(data, jsonFileReceiver)
+      for sim in range(1, Nsim + 1,1):
+        cmd = 'mkdir ./{}/BackTrafficv2/{}/{}/'.format(OutcomeFolder,Sim_name,sim)
         os.system(cmd)
-
         if Sim_name!="":
-          Config_Out="--out-folder-path={}".format("../../ns3_sim/" + Sim_name+ "/") 
-        with open("../ns-allinone-3.35/ns-3.35/scratch/ConfigScenario_template.json", "r") as jsonFileReceiver:
-          data = json.load(jsonFileReceiver)
-        print(data["Links"][1]["BackFeatures"]["DataRate"] )
-        data["Links"][1]["BackFeatures"]["DataRate"] = sim_rate
-
-        with open("../ns-allinone-3.35/ns-3.35/scratch/ConfigScenario.json", "w") as jsonFileReceiver:
-                    json.dump(data, jsonFileReceiver)
-        for sim in range(1, Nsim + 1,1):
-          cmd = 'mkdir ./{}/BackTrafficv2/{}/{}/'.format(OutcomeFolder,Sim_name,sim)
-          os.system(cmd)
-          if Sim_name!="":
-            Config_Out="--out-folder-path={}".format("../../scripts/{}/BackTrafficv2/" + Sim_name + "/"+ str(sim) + "/".format(OutcomeFolder)) 
-          RunNs3(ns3Path, ns3ScenarioC, ConfigPath, Config_Out, Config_Model)
+          Config_Out="--out-folder-path={}".format("../../scripts/{}/BackTrafficv2/" + Sim_name + "/"+ str(sim) + "/".format(OutcomeFolder)) 
+        RunNs3(ns3Path, ns3ScenarioC, ConfigPath, Config_Out, Config_Model)
 
       # StopNs3(ns3ScenarioC)
 
